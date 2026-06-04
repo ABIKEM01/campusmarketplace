2931dfb1423f6582170dccef6def3c153de82cae
vamundse <vamundse@gmail.com>
2026-06-04 13:57:13 +0200

    updated with extra validation rules

--- models/users.js
+++ models/users.js
@@ -4,22 +4,29 @@ const UserSchema = new mongoose.Schema(
 {
   name: {
     type: String,
-    required: true
+    required: [true, 'Name is required'],
+    min: [6, 'Name must be at least 6 letters long']
   },
 
   email: {
     type: String,
-    required: true,
-    unique: true
+    required: [true, 'Email is required'],
+    unique: true,
+    match: [/.+@.+\..+/, 'Provide an email in the format username@domain.com']
   },
 
   phone: {
     type: String,
-    required: true
+    required: [true, 'Phone number is required'],
+    min: [8, 'Phone number must be at least 8 characters']
   },
 
   role: {
     type: String,
+    enum: {
+      values: ['student', 'admin'],
+      message: 'Role must be student or admin'
+    },
     default: 'student'
   }
 },
